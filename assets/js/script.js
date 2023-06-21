88215cd89ae0dc542568252216abe82384f098fe
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-06-21 23:01:13 +1000

    added question array and progression

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,38 +3,44 @@ let timerCount = document.querySelector(".time-count");
 let mainHeading = document.querySelector(".main-heading");
 let subHeading = document.querySelector(".sub-heading");
 let answerSection = document.querySelector(".answer-section");
-let secondsLeft = 20;
+
+let secondsLeft = 90;
 let score = 0;
+let questionIndex = 0;
 
 function startGame() {
   let timerInterval = setInterval(function () {
     secondsLeft--;
     timerCount.textContent = secondsLeft;
 
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0) {
       clearInterval(timerInterval);
-      mainHeading.textContent = "Game Over";
-      answerSection.remove();
-
-      subHeading.innerHTML = "Enter your initials";
-      let submitScore = document.createElement("form");
-
-      let initials = document.createElement("input");
-      initials.setAttribute("type", "text");
-
-      let submitButton = document.createElement("input");
-      submitButton.setAttribute("type", "submit");
-
-      submitScore.appendChild(initials).appendChild(submitButton);
-      subHeading.appendChild(submitScore);
-
-      submitScore.addEventListener("submit", function (event) {
-        event.preventDefault();
-        window.location.href = "./assets/html/leaderboard.html";
-      });
+      gameOver();
     }
   }, 1000);
   question1();
+}
+
+function gameOver() {
+  mainHeading.textContent = "Game Over";
+  answerSection.remove();
+
+  subHeading.innerHTML = "Enter your initials";
+  let submitScore = document.createElement("form");
+
+  let initials = document.createElement("input");
+  initials.setAttribute("type", "text");
+
+  let submitButton = document.createElement("input");
+  submitButton.setAttribute("type", "submit");
+
+  submitScore.appendChild(initials).appendChild(submitButton);
+  subHeading.appendChild(submitScore);
+
+  submitScore.addEventListener("submit", function (event) {
+    event.preventDefault();
+    window.location.href = "./assets/html/leaderboard.html";
+  });
 }
 
 buttons.addEventListener("click", function () {
@@ -44,36 +50,75 @@ buttons.addEventListener("click", function () {
 });
 
 function question1() {
-  mainHeading.textContent = "Question 1";
+  mainHeading.textContent = questions[questionIndex].question;
 
-  let answer1 = document.createElement("button");
-  answer1.setAttribute("data-value", "wrong");
+  let answer1 = document.querySelector(".answer1");
+  answer1.style.display = "inline-block";
+  answer1.setAttribute("data-value", questions[questionIndex].choices[0]);
 
-  let answer2 = document.createElement("button");
-  answer2.setAttribute("data-value", "right");
+  let answer2 = document.querySelector(".answer2");
+  answer2.style.display = "inline-block";
+  answer2.setAttribute("data-value", questions[questionIndex].choices[1]);
 
-  let answer3 = document.createElement("button");
-  answer3.setAttribute("data-value", "wrong");
+  let answer3 = document.querySelector(".answer3");
+  answer3.style.display = "inline-block";
+  answer3.setAttribute("data-value", questions[questionIndex].choices[2]);
 
-  let answer4 = document.createElement("button");
-  answer4.setAttribute("data-value", "wrong");
+  let answer4 = document.querySelector(".answer4");
+  answer4.style.display = "inline-block";
+  answer4.setAttribute("data-value", questions[questionIndex].choices[3]);
 
-  answer1.textContent = "Wrong";
-  answer2.textContent = "Right";
-  answer3.textContent = "Wrong";
-  answer4.textContent = "Wrong";
+  answer1.innerHTML = questions[questionIndex].choices[0];
+  answer2.innerHTML = questions[questionIndex].choices[1];
+  answer3.innerHTML = questions[questionIndex].choices[2];
+  answer4.innerHTML = questions[questionIndex].choices[3];
 
-  answerSection.append(answer1, answer2, answer3, answer4);
+  // answerSection.append(answer1, answer2, answer3, answer4);
 }
 
 answerSection.addEventListener("click", function (event) {
   let element = event.target;
   let answerData = element.getAttribute("data-value");
 
-  if (element.matches("button") && answerData === "right") {
+  if (
+    element.matches("button") &&
+    answerData === questions[questionIndex].answer
+  ) {
     score += 10;
+    questionIndex++;
   }
-  if (element.matches("button") && answerData === "wrong") {
+  if (
+    element.matches("button") &&
+    answerData !== questions[questionIndex].answer
+  ) {
     secondsLeft -= 10;
   }
+  if (questionIndex === questions.length - 1) {
+    secondsLeft = 0;
+  }
+
+  question1();
 });
+
+const questions = [
+  {
+    question: "Question 1",
+    choices: ["wrong", "right", "wrong", "wrong"],
+    answer: "right",
+  },
+  {
+    question: "Question 2",
+    choices: ["right", "wrong", "wrong", "wrong"],
+    answer: "right",
+  },
+  {
+    question: "Question 3",
+    choices: ["wrong", "wrong", "right", "wrong"],
+    answer: "right",
+  },
+  {
+    question: "Question 4",
+    choices: ["wrong", "wrong", "wrong", "right"],
+    answer: "right",
+  },
+];
